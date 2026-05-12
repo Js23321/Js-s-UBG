ee2a44dd30890d40c8ff22d9e990e8317160e3fd
Js23321 <rao.junsheng@dhs.edu.sg>
2026-05-12 22:04:12 +0800

    update 2.0.0

--- games.js
+++ games.js
@@ -1,22 +1,22 @@
 const games = [
   {
     title: "Undertale Yellow",
-    url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/1.html",
+    url: "/games/1.html",
     image: "/images/1.jpg"
   },
   {
     title: "Geometry Dash",
-    url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/2.html",
+    url: "/games/2.html",
     image: "/images/2.jpg"
   },
   {
     title: "Stickman Duel",
-    url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/3.html",
+    url: "/games/3.html",
     image: "/images/3.jpg"
   },
   {
     title: "Space Waves",
-    url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/4.html",
+    url: "/games/4.html",
     image: "/images/4.jpg"
   }
 ];
